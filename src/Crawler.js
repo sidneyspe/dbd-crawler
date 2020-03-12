8721f22dbca0c30ab4d46f79b99acad817c89658
unknown <iago079>
2020-03-12 13:47:50 -0300

    trying get infos of tds

--- src/Crawler.js
+++ src/Crawler.js
@@ -15,16 +15,21 @@ class Crawler {
   async getItems() {
     const { data } = await axios.get(`${BASE_URL}/items`);
     const $ = cheerio.load(data);
-    const row = $('.wikitable tbody > tr');
+    const rows = $('.wikitable tbody > tr');
 
-    const count = 0;
-    $('.wikitable tbody > tr').each(() => {
-      console.log(
-        $(this)
-          .text()
-          .trim()
-      );
+    let count = 0;
+    const tds = $('.wikitable tbody > tr td');
+    tds.each((i, el) => {
+      count += 1;
+      const td_info = el;
+
+      if (td_info.attribs.col_span !== '5') {
+        if (count === 1)
+          console.log('Crawler -> getItems -> td_info', td_info.children.text);
+      }
     });
+
+    // console.log(td_info);
   }
 }
 
