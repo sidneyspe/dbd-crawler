24d407dac10443d915b28271037699805d1e3da9
Sidney Pimentel <sidneyspe@gmail.com>
2020-03-12 22:47:36 -0300

    updated crawler

--- src/Crawler.js
+++ src/Crawler.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 
 const BASE_URL = 'https://deadbydaylight.gamepedia.com';
+const FIRST_ROW = 0;
 
 class Crawler {
   constructor() {
@@ -12,39 +13,57 @@ class Crawler {
     this.getItems();
   }
 
+  removeLineBreak(string) {
+    return string.replace(/(\r\n|\n|\r)/gm, '');
+  }
+
+  getImgURL(el) {}
+
   async getItems() {
     const { data } = await axios.get(`${BASE_URL}/items`);
     const $ = cheerio.load(data);
 
-    const trs = $('.wikitable tbody > tr');
-    let count = 0;
+    let column_name = '';
     let column_names = [];
-    let list = []
+    let list = [];
 
-    trs.each((i, el) => {
-      count += 1;
-
+    const trs = $('.wikitable tbody > tr');
+    trs.each((row, el) => {
       const children = $(el).children();
       const qtt_columns = children.length;
-      let attr = {}
+      let attr = {};
+
       if (qtt_columns === 5) {
-        if (count === 1) {
-          children.each((i, el) => {
-            const column_name = $(el).text().toLowerCase();
+        if (row === FIRST_ROW) {
+          children.each((i, child) => {
+            column_name = $(child)
+              .text()
+              .toLowerCase();
             column_names.push(column_name);
-            console.log('Crawler -> getItems -> ', column_name);
+            console.log('Crawler -> getItems -> attr -> ', column_name);
           });
-        }else {
-          children.each((i, el) => {
-            const value = $(el).text();
-             attr[column_names[i]] = value;
+        } else {
+          children.each((i, child) => {
+            const text = $(child).text();
+            column_name = column_names[i];
+
+            if (column_name === 'icon') {
+              const img_url = $(child)
+                .find('img')
+                .attr('src');
+              attr[column_name] = img_url ? img_url : '';
+            } else if (column_name === 'name') {
+              attr[column_name] = this.removeLineBreak(text);
+            } else {
+              attr[column_name] = text;
+            }
           });
           list.push(attr);
         }
       }
     });
-    console.log('Crawler -> getItems -> column_names', column_names);
-    console.log('Crawler -> getItems -> list', list);
+    // console.log('Crawler -> getItems -> column_names', column_names);
+    // console.log('Crawler -> getItems -> list', list);
   }
 }
 
